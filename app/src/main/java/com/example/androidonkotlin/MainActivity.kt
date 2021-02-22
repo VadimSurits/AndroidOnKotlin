e4f4140674dd12c7866c13afd2ba0f14934a3c36
VadimSurits <vadimsurits@gmail.com>
2021-02-22 15:59:52 +1000

    ДЗ 1. Задание 4 выполено: 4. Добавить кнопку в разметку и повесить на неё clickListener в Activity.

--- app/src/main/java/com/example/androidonkotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidonkotlin/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.androidonkotlin
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        val button = findViewById<MaterialButton>(R.id.button)
+        button.setOnClickListener {
+            if (textView.text == getString(R.string.textview_text)) {
+                textView.text = getString(R.string.textview_kotlin_text)
+                textView.setTextColor(Color.WHITE)
+                textView.setBackgroundColor(Color.BLACK)
+            } else {
+                textView.setText(R.string.textview_text)
+                textView.setTextColor(Color.BLACK)
+                textView.setBackgroundColor(Color.WHITE)
+            }
+        }
     }
 }
