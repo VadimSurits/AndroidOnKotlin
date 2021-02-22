fcddce2cd0a9a69e062811f5ef338d8202b8beb3
VadimSurits <vadimsurits@gmail.com>
2021-02-22 18:32:36 +1000

    ДЗ 1. Задание 5c выполнено. 5c. выводите значения из разных циклов в консоль, используя примеры из методических материалов.

--- app/src/main/java/com/example/androidonkotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidonkotlin/MainActivity.kt
@@ -3,9 +3,15 @@ package com.example.androidonkotlin
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+
+const val TAG_A = "ForEachCycleTag"
+const val TAG_B = "ClosedRangeCycleTag"
+const val TAG_C = "CRCReversiveTag"
+const val TAG_D = "CRCReversiveStep5Tag"
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
@@ -20,10 +26,12 @@ class MainActivity : AppCompatActivity() {
                 textView.text = getString(R.string.textview_kotlin_text)
                 textView.setTextColor(Color.WHITE)
                 textView.setBackgroundColor(Color.BLACK)
+                forEachCycle()
             } else {
                 textView.setText(R.string.textview_text)
                 textView.setTextColor(Color.BLACK)
                 textView.setBackgroundColor(Color.WHITE)
+                closedRangeCycle()
             }
         }
 
@@ -33,10 +41,37 @@ class MainActivity : AppCompatActivity() {
         button.setOnLongClickListener {
             if (textView.text != "${testDataClass.string} ${testDataClass.number}") {
                 textView.setText("${testDataClass.string} ${testDataClass.number}")
+                closedRangeCycleReversive()
             } else {
                 textView.setText("${testObject.string} ${testObject.number}")
+                closedRangeCycleReversiveWithStep()
             }
             true
+        }
+    }
+
+    fun forEachCycle() {
+        val arrayList = arrayListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        for (i in arrayList) {
+            Log.d(TAG_A, i.toString())
+        }
+    }
+
+    fun closedRangeCycle() {
+        for (i in 1..50) {
+            Log.d(TAG_B, i.toString())
+        }
+    }
+
+    fun closedRangeCycleReversive() {
+        for (i in 100 downTo 50) {
+            Log.d(TAG_C, i.toString())
+        }
+    }
+
+    fun closedRangeCycleReversiveWithStep() {
+        for (i in 50 downTo 0 step 5) {
+            Log.d(TAG_D, i.toString())
         }
     }
 }
