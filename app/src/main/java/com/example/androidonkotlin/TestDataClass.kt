c7e68fadbb22cfddc0e5efea892eb9fe82743d1d
VadimSurits <vadimsurits@gmail.com>
2021-03-02 20:02:08 +1000

    ДЗ 2 Выполнено.

--- app/src/main/java/com/example/androidonkotlin/TestDataClass.kt
+++
@@ -1,4 +0,0 @@
-package com.example.androidonkotlin
-
-data class TestDataClass(val string: String, val number: Int) {
-}
---
+++ app/src/main/java/com/example/androidonkotlin/model/City.kt
@@ -0,0 +1,7 @@
+package com.example.androidonkotlin.model
+
+data class City(
+        val cityName: String,
+        val lat: Double,
+        val lon: Double
+)
